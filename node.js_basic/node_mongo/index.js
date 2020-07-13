e6336ad9598ac86a9af2e41a29e8cdb66fa4d1a3
PriontoAbdullah <priontosoft@gmail.com>
2020-07-13 23:37:18 +0600

    MongoDB Added

--- node.js_basic/node_mongo/index.js
+++ node.js_basic/node_mongo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
 const app = express();
 
@@ -9,13 +11,47 @@ app.use(bodyParser.json());
 
 const users = ['Jodu', 'Modu', 'Kodu', 'Mofiz'];
 
+//database connection
+const dbUser = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const uri = `mongodb+srv://${dbUser}:${pass}@cluster0.xw1ix.mongodb.net/test?retryWrites=true&w=majority`;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+// Manual database connection
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection
+//     collection.insertOne({
+//         name: 'Samsung',
+//         price: 19990,
+//         stock: 52
+//     }, (er, res) => {
+//         console.log('Successfully inserted');
+//     })
+//     console.log('Database connected...');
+//     client.close();
+// });
+
+
 // get request
-app.get('/', (req, res) => {
-    const fruit = {
-        product: 'mango',
-        price: 220
-    }
-    res.send(fruit);
+app.get('/products', (req, res) => {
+    // connect server again
+    client = new MongoClient(uri, { useNewUrlParser: true });
+
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection
+        collection.find().limit(5).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                // console.log('Successfully inserted', result);
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
 });
 
 app.get('/fruits/banana', (req, res) => {
@@ -30,13 +66,30 @@ app.get('/users/:id', (req, res) => {
 })
 
 //post request
-app.post('/addUser', (req, res) => {
+app.post('/addProduct', (req, res) => {
     // console.log('Data Received', req.body);
     //save to database
-    const user = req.body;
-    user.id = 52;
-    res.send(user);
-})
+    const product = req.body;
+    console.log(product);
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+    // connect server again
+    client = new MongoClient(uri, { useNewUrlParser: true });
 
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection
+        collection.insertOne(product, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                // console.log('Successfully inserted', result);
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening to port 3000'));
