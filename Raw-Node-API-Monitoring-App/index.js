915dd3fd9275bbafae54aecaf75cc949ccc209ec
PriontoAbdullah <priontosoft@gmail.com>
2021-02-19 23:53:49 +0600

    Create a server and use postman

--- Raw-Node-API-Monitoring-App/index.js
+++ Raw-Node-API-Monitoring-App/index.js
@@ -1,8 +1,35 @@
 /*
  * Title: Uptime Monitoring Application
  * Description: A RESTFul API to monitoring up or down time of user defined links
- * Author: Prionto
+ * Author: Prionto Abdullah
  * Date: 19/02/21
  *
  */
 
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+	port: 3000
+};
+
+// create server
+app.createServer = () => {
+	const server = http.createServer(app.handleReqRes);
+	server.listen(app.config.port, () => {
+		console.log(`server listening on port ${app.config.port}`);
+	});
+};
+
+// handle request response
+app.handleReqRes = (req, res) => {
+	// response handle
+	res.end('Hello world!');
+};
+
+// start the server
+app.createServer();
