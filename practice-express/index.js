1f814a2f98931371c6dfebfd477c23601c85fe62
PriontoAbdullah <priontosoft@gmail.com>
2021-02-28 21:41:26 +0600

    Express js practice

--- practice-express/index.js
+++ practice-express/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/about', (req, res) => {
 	res.send(`<h1> About Page </h1>`);
 });
@@ -14,6 +16,11 @@ app.get('/help', (req, res) => {
 
 app.get('/', (req, res) => {
 	res.send(`<h1> Hello World </h1>`);
+});
+
+app.post('/', (req, res) => {
+	console.log(req.body.name);
+	res.send('This is home page with post request');
 });
 
 app.get('*', (req, res) => {
