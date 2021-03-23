f63fdb456a73dca46b5f2641995fe66aa488aa2b
PriontoAbdullah <priontosoft@gmail.com>
2021-03-23 18:45:50 +0600

    Multer file handeling

--- Node-mongo-CRUD/index.js
+++ Node-mongo-CRUD/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+const multer = require('multer');
+const path = require('path');
+
+const upload_folder = './uploads/';
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +20,38 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
+// define the storage
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, upload_folder);
+	},
+	filename: (req, file, cb) => {
+		const fileExt = path.extname(file.originalname);
+		const filename = file.originalname.replace(fileExt, '').toLowerCase().split(' ').join('-') + '-' + Date.now();
+
+		cb(null, filename, fileExt);
+	}
+});
+
+// prepare the final multer upload object
+const upload = multer({
+	storage: storage,
+	limit: {
+		fileSize: 1000000 // 1MB
+	},
+	fileFilter: (req, file, cb) => {
+		if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+			cb(null, true);
+		} else {
+			cb(new Error('Only .jpg .jpeg .png formate allowed'));
+		}
+	}
+});
+
+app.post('/', upload.single('avatar'), (req, res) => {
+	res.send('Hello world');
+});
+
 client.connect((err) => {
 	const productCollection = client.db('organicdb').collection('products');
 
@@ -24,7 +60,6 @@ client.connect((err) => {
 			res.send(documents);
 		});
 	});
-
 
 	app.get('/product/:id', (req, res) => {
 		productCollection.find({ _id: ObjectId(req.params.id) }).toArray((err, documents) => {
@@ -55,9 +90,24 @@ client.connect((err) => {
 				}
 			)
 			.then((result) => {
-                res.send(result.modifiedCount > 0);
-            });
+				res.send(result.modifiedCount > 0);
+			});
 	});
 });
 
-app.listen(5000);
+// default error handler
+app.use((err, req, res, next) => {
+	if (err) {
+		if (err instanceof multer.MulterError) {
+			res.sendStatus(500).send('There was an upload error');
+		} else {
+			res.sendStatus(500).send(err.message);
+		}
+	} else {
+		res.send('Success!');
+	}
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
